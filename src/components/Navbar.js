602ebaf17c0fe0c39d76c2ea0d1af1f8194acc05
Sarthakbhuptani123 <mrsarthak825@gmail.com>
2025-07-11 18:23:00 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,7 @@ const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-black text-white shadow-lg z-50">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="flex items-center gap-2">
-  <img
-    src={profile}
-    alt="Sarthak"
-    className="w-8 h-8 rounded-full object-cover"
-  />
-  <span className="text-lg font-bold">Sarthak</span>
-</div>
-
-
+  
         <ul className="flex gap-3">
           <li><a href="#home" className="hover:text-blue-400">Home</a></li>
           <li><a href="#about" className="hover:text-blue-400">About</a></li>
