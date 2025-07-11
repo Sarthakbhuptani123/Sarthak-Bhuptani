62c27d30aea142a443d1e24169ee0c5c76c72df5
Sarthakbhuptani123 <mrsarthak825@gmail.com>
2025-07-11 18:31:35 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,15 +15,12 @@ const NavBar = () => {
 </div>
 
 
-        <ul className="flex gap-3">
+        <ul className="flex gap-6">
           <li><a href="#home" className="hover:text-blue-400">Home</a></li>
           <li><a href="#about" className="hover:text-blue-400">About</a></li>
           <li><a href="#skills" className="hover:text-blue-400">Skills</a></li>
           <li><a href="#projects" className="hover:text-blue-400">Projects</a></li>
           <li><a href="#contact" className="hover:text-blue-400">Contact</a></li>
-          <li>
-            
-          </li>
         </ul>
       </div>
     </nav>
